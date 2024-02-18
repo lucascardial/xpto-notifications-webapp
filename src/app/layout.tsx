ecacc8dc6c003bae0df02165d35278fb90220796
cardial <luccascardial@gmail.com>
2024-02-18 17:48:59 -0300

    feat: react query provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+import { AppRouterCacheProvider }from '@mui/material-nextjs/v13-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import theme from '@/theme';
+import { ErrorUiProvider } from '@/providers/ErrorUiProvider/ErrorUiProvider';
+import TanStackQueryProvider from '@/providers/QueryClientProvider';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <AppRouterCacheProvider>
+        <ThemeProvider theme={theme}>
+          <TanStackQueryProvider>
+            {children}
+          </TanStackQueryProvider>
+          <ErrorUiProvider />
+        </ThemeProvider>
+      </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
