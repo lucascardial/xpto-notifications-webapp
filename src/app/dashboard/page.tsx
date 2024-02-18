77846a1e1743764e0bf5096b04c66018077f0ef5
cardial <luccascardial@gmail.com>
2024-02-18 20:33:34 -0300

    refactor: load data from server

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,68 +2,15 @@ import { Box, Button, Container, Paper, Table, TableBody, TableCell, TableContai
 import UploadCsvForm from "./forms/UploadCsvForm";
 import ContactImporsTable from "./components/ContactImportsTable";
 
-const importacoes = [
-  {
-    id: Math.random().toString(36).substr(2, 9),
-    name: 'Importação 1',
-    status: 'Concluída',
-    date: '18/02/2024 14:00'
-  },
-  {
-    id: Math.random().toString(36).substr(2, 9),
-    name: 'Importação 2',
-    status: 'Concluída',
-    date: '18/02/2024 14:00'
-  },
-  {
-    id: Math.random().toString(36).substr(2, 9),
-    name: 'Importação 3',
-    status: 'Concluída',
-    date: '18/02/2024 14:00'
-  },
-  {
-    id: Math.random().toString(36).substr(2, 9),
-    name: 'Importação 4',
-    status: 'Concluída',
-    date: '18/02/2024 14:00'
-  },
-  {
-    id: Math.random().toString(36).substr(2, 9),
-    name: 'Importação 5',
-    status: 'Concluída',
-    date: '18/02/2024 14:00'
-  },
-  {
-    id: Math.random().toString(36).substr(2, 9),
-    name: 'Importação 6',
-    status: 'Concluída',
-    date: '18/02/2024 14:00'
-  },
-  {
-    id: Math.random().toString(36).substr(2, 9),
-    name: 'Importação 7',
-    status: 'Concluída',
-    date: '18/02/2024 14:00'
-  },
-  {
-    id: Math.random().toString(36).substr(2, 9),
-    name: 'Importação 8',
-    status: 'Concluída',
-    date: '18/02/2024 14:00'
-  },
-  {
-    id: Math.random().toString(36).substr(2, 9),
-    name: 'Importação 9',
-    status: 'Concluída',
-    date: '18/02/2024 14:00'
-  },
-  {
-    id: Math.random().toString(36).substr(2, 9),
-    name: 'Importação 10',
-    status: 'Concluída',
-    date: '18/02/2024 14:00'
+async function getData() {
+  const res = await fetch('http://127.0.0.1:8000/api/v1/contacts/imports', { cache: 'no-store' })
+ 
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
   }
-];
+ 
+  return res.json()
+}
 
 const NotificacaoPage = async() => {
   const contactImports = await getData();
@@ -79,15 +26,7 @@ const NotificacaoPage = async() => {
         </Container>
     );
 }
-async function getData() {
-  const res = await fetch('http://127.0.0.1:8000/api/v1/contacts/imports', { cache: 'no-store' })
- 
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
- 
-  return res.json()
-}
+
 
 
 export default NotificacaoPage;
