7ec6a7012a34d84d3bdc77e2be3432fd973013ac
cardial <luccascardial@gmail.com>
2024-02-18 20:31:29 -0300

    refactor: export query client

--- src/providers/QueryClientProvider/index.tsx
+++ src/providers/QueryClientProvider/index.tsx
@@ -3,10 +3,10 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 
-export default function TanStackQueryProvider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = React.useState(() => new QueryClient())
+export const reactQueryClient = new QueryClient()
 
+export default function TanStackQueryProvider({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={reactQueryClient}>{children}</QueryClientProvider>
   )
 }
